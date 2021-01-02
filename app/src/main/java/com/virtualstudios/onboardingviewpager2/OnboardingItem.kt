6c7674d36f8b65f16bc1da14acdc881f213b3cdb
virendersran01 <virendersran01@gmail.com>
2021-01-02 17:20:46 +0530

    Initial commit

---
+++ app/src/main/java/com/virtualstudios/onboardingviewpager2/OnboardingItem.kt
@@ -0,0 +1,7 @@
+package com.virtualstudios.onboardingviewpager2
+
+data class OnboardingItem(
+    val onboardingImage: Int,
+    val title: String,
+    val description: String
+)
---
+++ app/src/main/java/com/virtualstudios/onboardingviewpager2/OnboardingItemsAdapter.java
@@ -0,0 +1,18 @@
+package com.virtualstudios.onboardingviewpager2;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+class OnboardingItemsAdapter {
+
+    inner class OnboardingItemViewHolder(view:View) : RecyclerView.ViewHolder(view){
+
+        private val imageOnboarding = view.findViewById<ImageView>(R.id.imageOnboarding)
+        private val textTitle = view.findViewById<TextView>(R.id.textTitle)
+        private val textDescription = view.findViewById<TextView>(R.id.textDescription)
+
+    }
+}
